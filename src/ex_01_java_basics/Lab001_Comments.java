09b0682465ae5060ec4ed47eb656ed458a6cf443
BuntyRepo <bunty1947singh@gmail.com>
2025-01-09 17:29:59 +0530

    My second commit

---
+++ src/ex_01_java_basics/Lab001_Comments.java
@@ -0,0 +1,9 @@
+package ex_01_java_basics;
+
+public class Lab001_Comments {
+    public static void main(String[] args){
+        //writting a code with the comments
+        System.out.println("My name is Bunty");
+    }
+
+}
