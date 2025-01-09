5d4923345dd134f5adf05e078fb46bbeb33c3602
BuntyRepo <bunty1947singh@gmail.com>
2025-01-09 17:52:08 +0530

    My third commit

---
+++ src/ex_01_java_basics/Lab002_keywords.java
@@ -0,0 +1,11 @@
+package ex_01_java_basics;
+
+public class Lab002_keywords {
+    //Printing message of keywords used in the programs
+    public static void main(String[] args){
+        System.out.println("package");
+        System.out.println("public");
+        System.out.println("class");
+        System.out.println("void");
+    }
+}
