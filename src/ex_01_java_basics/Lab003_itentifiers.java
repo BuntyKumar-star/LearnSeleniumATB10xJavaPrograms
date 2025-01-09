ad9ee554b4a56d0ff86075d21016348e451c6979
BuntyRepo <bunty1947singh@gmail.com>
2025-01-09 18:22:00 +0530

    My 4th commit

---
+++ src/ex_01_java_basics/Lab003_itentifiers.java
@@ -0,0 +1,8 @@
+package ex_01_java_basics;
+
+public class Lab003_itentifiers {
+    // printing message with identifiers in the program
+    public static void main(String[] args){
+        System.out.println("itentifiers");
+    }
+}
