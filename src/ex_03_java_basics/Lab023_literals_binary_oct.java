c1abf5160573d30e21bfe945de67c022b1eee5be
BuntyRepo <bunty1947singh@gmail.com>
2025-01-24 11:09:54 +0530

    My 9th commit

---
+++ src/ex_03_java_basics/Lab023_literals_binary_oct.java
@@ -0,0 +1,20 @@
+package ex_03_java_basics;
+
+public class Lab023_literals_binary_oct {
+    public static void main(String[] args){
+        int age =85 ; //Decimal System with base 10
+        //Binary Lietrals
+        int binary_num = 0b1010;//Decimal system with base 10
+        //octal base
+        int octa = 0101; // octa base 8
+        //Hexadecimal Lietrals
+        int hexa = 0Xface; //hexa base 16
+        // color combination are in hexa
+        System.out.println(age);
+        System.out.println(binary_num);
+        System.out.println(octa);
+        System.out.println(hexa);
+
+
+    }
+}
