1cb92121928a49bede0a087c54152beb927c20ee
BuntyRepo <bunty1947singh@gmail.com>
2025-02-07 16:32:58 +0530

    My 14th commit

---
+++ src/ex_01_java_basics/ShortCutHW.java
@@ -0,0 +1,10 @@
+package ex_01_java_basics;
+
+public class ShortCutHW {
+    public static void main(String[] args){
+        System.out.println("Anmol");
+    }
+    public static void main(){
+        System.out.println("Anmol");
+    }
+}
---
+++ src/ex_06_java_Ternanry_operators/Lab052_Ternary_operators.java
@@ -0,0 +1,9 @@
+package ex_06_java_Ternanry_operators;
+
+public class Lab052_Ternary_operators {
+    public static void main(String[] args){
+        int a = -5;
+        String result = a>0 ? "Positive" : "Negative";
+        System.out.println(result);
+    }
+}
